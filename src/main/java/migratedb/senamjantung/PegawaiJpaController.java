7171617bf03fa94b48d920d5b1db765acd6d9edd
Zahran Rafif <zahranlumia@gmail.com>
2022-10-18 19:05:07 +0700

    Menghubungkan EntityManagerFactory PegawaiJpaController

--- src/main/java/migratedb/senamjantung/PegawaiJpaController.java
+++ src/main/java/migratedb/senamjantung/PegawaiJpaController.java
@@ -14,6 +14,7 @@ import java.util.Collection;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import migratedb.senamjantung.exceptions.IllegalOrphanException;
 import migratedb.senamjantung.exceptions.NonexistentEntityException;
 import migratedb.senamjantung.exceptions.PreexistingEntityException;
@@ -27,7 +28,11 @@ public class PegawaiJpaController implements Serializable {
     public PegawaiJpaController(EntityManagerFactory emf) {
         this.emf = emf;
     }
-    private EntityManagerFactory emf = null;
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("migratedb_senamjantung_jar_0.0.1-SNAPSHOTPU");
+
+    public PegawaiJpaController() {
+    }
+    
 
     public EntityManager getEntityManager() {
         return emf.createEntityManager();
