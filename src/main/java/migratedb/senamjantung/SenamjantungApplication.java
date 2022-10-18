accf44cd1e6e766153f88350f5d94d1aba21be97
Zahran Rafif <zahranlumia@gmail.com>
2022-10-18 18:49:11 +0700

    First Commit

---
+++ src/main/java/migratedb/senamjantung/SenamjantungApplication.java
@@ -0,0 +1,13 @@
+package migratedb.senamjantung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenamjantungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenamjantungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/migratedb/senamjantung/SenamjantungApplicationTests.java
@@ -0,0 +1,13 @@
+package migratedb.senamjantung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenamjantungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
