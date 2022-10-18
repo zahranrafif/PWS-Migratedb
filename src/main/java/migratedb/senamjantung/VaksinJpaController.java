6809d58b289a6f211cd432720822fadaaf0ca2e6
Zahran Rafif <zahranlumia@gmail.com>
2022-10-18 20:25:11 +0700

    Menghubungkan EntityManagerFactory VaksinJpaController

--- src/main/java/migratedb/senamjantung/VaksinJpaController.java
+++ src/main/java/migratedb/senamjantung/VaksinJpaController.java
@@ -14,6 +14,7 @@ import java.util.Collection;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import migratedb.senamjantung.exceptions.IllegalOrphanException;
 import migratedb.senamjantung.exceptions.NonexistentEntityException;
 import migratedb.senamjantung.exceptions.PreexistingEntityException;
@@ -27,7 +28,11 @@ public class VaksinJpaController implements Serializable {
     public VaksinJpaController(EntityManagerFactory emf) {
         this.emf = emf;
     }
-    private EntityManagerFactory emf = null;
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("migratedb_senamjantung_jar_0.0.1-SNAPSHOTPU");
+
+    public VaksinJpaController() {
+    }
+    
 
     public EntityManager getEntityManager() {
         return emf.createEntityManager();
