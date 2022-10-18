9f1ce198d0b308b5974bb4a6ff20eb12fcff3f60
Zahran Rafif <zahranlumia@gmail.com>
2022-10-18 19:00:24 +0700

    Menghubungkan EntityManagerFactory KucingJpaController

--- src/main/java/migratedb/senamjantung/KucingJpaController.java
+++ src/main/java/migratedb/senamjantung/KucingJpaController.java
@@ -14,6 +14,7 @@ import java.util.Collection;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import migratedb.senamjantung.exceptions.IllegalOrphanException;
 import migratedb.senamjantung.exceptions.NonexistentEntityException;
 import migratedb.senamjantung.exceptions.PreexistingEntityException;
@@ -27,7 +28,11 @@ public class KucingJpaController implements Serializable {
     public KucingJpaController(EntityManagerFactory emf) {
         this.emf = emf;
     }
-    private EntityManagerFactory emf = null;
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("migratedb_senamjantung_jar_0.0.1-SNAPSHOTPU");
+
+    public KucingJpaController() {
+    }
+    
 
     public EntityManager getEntityManager() {
         return emf.createEntityManager();
